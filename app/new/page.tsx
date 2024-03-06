d9b84f2d14080e9992e23a5063eb3b115de28bba
Mohammad Chohan <mohammadchohan6f@gmail.com>
2024-03-07 03:11:47 +1100

    make page responsive

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import donationBox from "../../public/donate-bg.png";
+import donationBox from "@/public/donate-box.webp";
+import swinisLogo from "@/public/swinis.png";
 import { MdEmail, MdPhone, MdWhatsapp } from "react-icons/md";
 import { FaFacebook, FaInstagram, FaLinkedin, FaYoutube } from "react-icons/fa";
 
@@ -22,15 +23,15 @@ export default function Page() {
   return (
     <>
       <nav>
-        <ul className="font-signika flex px-[132px] py-4 justify-between items-center text-xl font-bold">
+        <ul className="flex items-center justify-between p-2 font-signika lg:px-[132px] lg:text-xl lg:font-bold">
           <li>Home</li>
           <li>Library</li>
           <li>
             <Image
-              src="/swinis.png"
+              src={swinisLogo}
               alt="Swinburne Islamic Society Logo"
-              width={180}
-              height={84}
+              width={135}
+              height={63}
             />
           </li>
           <li>Gallery</li>
@@ -38,30 +39,30 @@ export default function Page() {
         </ul>
       </nav>
       <main>
-        <div className="flex">
-          <div className="bg-[url('/white-mosque.png')] flex flex-col items-center w-[897px] grow h-[665px] bg-center bg-no-repeat bg-cover pt-[42px] md:px-32">
+        <div className="lg:flex">
+          <div className="flex h-[665px] grow flex-col items-center bg-[url('/white-mosque.png')] bg-cover bg-center bg-no-repeat pt-[42px] md:px-32 lg:w-[897px]">
             <Image src="/shahada.png" alt="Shahada" width={218} height={94} />
-            <h1 className="font-garamond text-center font-medium text-6xl text-[#47341C]">
+            <h1 className="text-center font-garamond text-6xl font-medium text-[#47341C]">
               Welcome to Swinburne Islamic Society
             </h1>
           </div>
-          <div className="flex flex-col bg-[url('/patterned-blue.png')] items-center pt-[18px] text-white px-9 max-w-[450px]">
+          <div className="flex flex-col items-center bg-[url('/patterned-blue.png')] p-4 px-9 text-white lg:max-w-[450px]">
             <Image
               src="/allah.png"
               width={120}
               height={56}
               alt="Allah's name written in Arabic"
             />
-            <div className="text-4xl text-[#C59A5D] font-[850] pb-2">
+            <div className="pb-2 text-4xl font-[850] text-[#C59A5D]">
               Prayer Time
             </div>
-            <div className="text-xl pb-3">Prayer time in Swinburne Musalla</div>
+            <div className="pb-3 text-xl">Prayer time in Swinburne Musalla</div>
             <table className="text-xl">
               <thead>
                 <tr className="text-[#C59A5D]">
                   <th></th>
-                  <th className="px-3 font-[850] text-center italic">Adhaan</th>
-                  <th className="px-3 font-[850] text-center italic">
+                  <th className="px-3 text-center font-[850] italic">Adhaan</th>
+                  <th className="px-3 text-center font-[850] italic">
                     Iqaamah
                   </th>
                 </tr>
@@ -74,10 +75,10 @@ export default function Page() {
                   return (
                     <tr key={time.prayer} className={`${border}`}>
                       <td className="py-2 font-[850]">{time.prayer}</td>
-                      <td className="px-3 py-2 font-[850] text-center">
+                      <td className="px-3 py-2 text-center font-[850]">
                         {time.adhaan}
                       </td>
-                      <td className="px-3 py-2 font-[850] text-center">
+                      <td className="px-3 py-2 text-center font-[850]">
                         {time.iqaamah}
                       </td>
                     </tr>
@@ -85,19 +86,26 @@ export default function Page() {
                 })}
               </tbody>
             </table>
-            <div className="text-center text-sm pt-4">
+            <div className="pt-4 text-center text-sm">
               GS308 (Brothers), GS305 (Sisters) | Level 3, George Building (GS),
               Swinburne University of Technology (Hawthorn Campus )
             </div>
           </div>
         </div>
-        <div className="flex">
-          <div className="flex flex-col justify-center items-center gap-4 py-8 bg-[#104766] text-white grow">
-            <h2 className="text-2xl text-[#C59A5D] font-bold">
+        <div className="lg:flex lg:flex-row-reverse">
+          <Image
+            src="/welcome-back.png"
+            width={450}
+            height={450}
+            className="w-full lg:w-auto"
+            alt="Welcome Back Event Poster"
+          />
+          <div className="flex grow flex-col items-center justify-center gap-4 bg-[#104766] py-8 text-white">
+            <h2 className="text-2xl font-bold text-[#C59A5D]">
               Upcoming Event
             </h2>
             <p>Welcome Back Week First Sem 2024</p>
-            <div className="flex flex-row text-2xl text-[#144560] gap-1">
+            <div className="flex flex-row gap-1 text-2xl text-[#144560]">
               {Object.entries(countdown).map(([key, value], index, array) => (
                 <>
                   <div key={key} className="flex flex-col items-center">
@@ -107,13 +115,13 @@ export default function Page() {
                         .split("")
                         .map((num, i) => {
                           return (
-                            <span key={i} className="bg-[#D9D9D9] p-1 rounded">
+                            <span key={i} className="rounded bg-[#D9D9D9] p-1">
                               {num}
                             </span>
                           );
                         })}
                     </div>
-                    <div className="text-white uppercase text-sm font-bold">
+                    <div className="text-sm font-bold uppercase text-white">
                       {key}
                     </div>
                   </div>
@@ -123,64 +131,66 @@ export default function Page() {
                 </>
               ))}
             </div>
-            <button className="bg-[#95B0C9] text-[#144560] italic text-xl p-1 px-2 rounded font-medium">
+            <button className="rounded bg-[#95B0C9] p-1 px-2 text-xl font-medium italic text-[#144560]">
               All Events
             </button>
           </div>
-          <Image
-            src="/welcome-back.png"
-            width={450}
-            height={450}
-            alt="Welcome Back Event Poster"
-          />
         </div>
-        <div className="relative">
-          <Image src={donationBox} alt="Donation Box" />
-          <div className="flex flex-col items-end p-4 lg:p-14 lg:py-16 text-2xl lg:gap-8 ml-auto w-[50%] gap-4 z-20 absolute top-0 right-0">
-            <h2 className="text-4xl font-bold text-[#144560]">Support Us</h2>
-            <p>
+        <div className="flex h-full bg-gradient-to-tr from-[#f2edea] via-[#e5d5c8] via-90% to-[#d2b7a2] p-4 lg:p-8">
+          <Image
+            src={donationBox}
+            alt="Donation Box"
+            className="w-fit self-end"
+          />
+          <div className="ml-auto flex w-[50%] flex-col items-end gap-4 lg:gap-8 lg:text-2xl">
+            <h2 className="text-2xl font-bold text-[#144560] lg:text-4xl">
+              Support Us
+            </h2>
+            <p className="text-justify">
               Prophet Muhammad ﷺ said: &quot;The believer&apos;s shade on the
               Day of Resurrection will be their charity.&quot;
             </p>
             <p className="self-center">(Tirmidhi)</p>
-            <button className="self-center text-[#144560] bg-white italic p-1 px-4 rounded hover:underline">
+            <button className="self-center rounded bg-white p-1 px-4 italic text-[#144560] hover:underline">
               Donate Now
             </button>
           </div>
         </div>
-        <div className="bg-[url('/shapes.png')] p-10 bg-[#95B0C9] text-2xl font-bold">
-          <h2 className="text-white text-3xl font-bold">Recent Event Reels</h2>
-          <div className="flex justify-center gap-32 pt-4">
-            <div className="h-40 bg-[#D9D9D9] rounded-3xl px-32 py-64">
+        <div className="bg-[#868686] p-3"></div>
+        <div className="bg-[#646464] p-3"></div>
+        <div className="bg-[#95B0C9] bg-[url('/shapes.png')] p-5 text-lg font-bold lg:p-10 lg:text-2xl">
+          <h2 className="text-2xl font-bold text-white lg:text-3xl">
+            Recent Event Reels
+          </h2>
+          <div className="flex items-start justify-around gap-4 pt-4 lg:gap-32">
+            <div className="flex aspect-[4/10] w-[20%] flex-col items-center justify-center rounded-3xl bg-[#D9D9D9]">
               Reel 1
             </div>
-            <div className="h-40 bg-[#D9D9D9] rounded-3xl  px-32 py-64">
+            <div className="flex aspect-[4/10] w-[20%] flex-col items-center justify-center rounded-3xl bg-[#D9D9D9]">
               Reel 2
             </div>
-            <div className="h-40 bg-[#D9D9D9] rounded-3xl  px-32 py-64">
+            <div className="flex aspect-[4/10] w-[20%] flex-col items-center justify-center rounded-3xl bg-[#D9D9D9]">
               Reel 3
             </div>
           </div>
         </div>
-        <div className="bg-[url('/shapes.png')] p-10 bg-[#144560] text-2xl font-bold text-white">
-          <h2 className="text-[#C59A5D] text-3xl font-bold text-end">
-            Our Services
-          </h2>
-          <div className="flex justify-center gap-16 pt-4">
-            <div className="h-40 rounded-3xl px-32 py-64 bg-[url('/fancy-shape.png')] bg-cover bg-no-repeat">
+        <div className="bg-[#144560] bg-[url('/shapes.png')] p-5 text-lg font-bold text-white lg:p-10 lg:text-2xl">
+          <h2 className="text-end text-3xl text-[#C59A5D]">Our Services</h2>
+          <div className="flex items-start justify-around pt-4">
+            <div className="flex aspect-[2/3] w-[25%] flex-col items-center justify-center rounded-3xl bg-[url('/fancy-shape.png')] bg-cover bg-no-repeat">
               Generic
             </div>
-            <div className="h-40 rounded-3xl mt-20 px-32 py-64 bg-[url('/fancy-shape.png')] bg-cover bg-no-repeat">
+            <div className="mt-10 flex aspect-[2/3] w-[25%] flex-col items-center justify-center rounded-3xl bg-[url('/fancy-shape.png')] bg-cover bg-no-repeat lg:mt-20">
               Generic
             </div>
-            <div className="h-40 rounded-3xl px-32 mt-40 py-64 bg-[url('/fancy-shape.png')] bg-cover bg-no-repeat">
+            <div className="mt-20 flex aspect-[2/3] w-[25%] flex-col items-center justify-center rounded-3xl bg-[url('/fancy-shape.png')] bg-cover bg-no-repeat lg:mt-40">
               Generic
             </div>
           </div>
         </div>
       </main>
-      <footer className="bg-[#95B0C9] p-8 text-white">
-        <div className="flex gap-32">
+      <footer className="bg-[#95B0C9] p-4 text-sm text-white lg:p-8">
+        <div className="flex gap-4 lg:gap-32">
           <div>
             <h2 className="text-3xl font-bold text-[#144560]">About Us</h2>
             <div className="pt-2">
@@ -218,7 +228,7 @@ export default function Page() {
             </div>
           </div>
         </div>
-        <div className="text-center pt-6">
+        <div className="pt-6 text-center">
           All Right Reserved @Swinburne Islamic Society
         </div>
       </footer>
