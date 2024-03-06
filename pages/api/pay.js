9f1bf469d89934b5fb2fb7b17918b338230666e6
Mohammad Chohan <mohammadchohan6f@gmail.com>
2024-03-06 22:50:46 +1100

    removed console.logs and formatted

--- pages/api/pay.js
+++ pages/api/pay.js
@@ -1,13 +1,12 @@
 import { Client } from "square";
 import { randomUUID } from "crypto";
-console.log(process.env.SQUARE_ACCESS_TOKEN);
+
 const { paymentsApi } = new Client({
   accessToken: process.env.SQUARE_ACCESS_TOKEN,
   environment: "sandbox",
 });
 
 export default async function handler(req, res) {
-  console.log("API triggered");
   if (req.method === "POST") {
     const { result } = await paymentsApi.createPayment({
       idempotencyKey: randomUUID(),
@@ -17,7 +16,6 @@ export default async function handler(req, res) {
         amount: 100,
       },
     });
-    console.log(result);
     res.status(200).json(result);
   } else {
     res.status(500).send();
