e5ab29c87f2a766dee587cf71c71dec2b4a970b9
Mohammad Chohan <mohammadchohan6f@gmail.com>
2024-03-31 02:59:41 +1100

    move app/new/page.tsx to app/page.tsx, replace adhan with adhan-clock, use Intl API instead of luxon

--- lib/prayerTimes.ts
+++ lib/prayerTimes.ts
@@ -1,26 +1,5 @@
-import { NumberPrayerTimes } from "adhan-clock";
-import { Firestore, doc, getDoc } from "firebase/firestore";
-
-export function getIqamahTimes(
-  prayTimes: NumberPrayerTimes,
-  hardcodedTimes: any,
-  offsetTimes: any,
-) {
-  const prayerNames = [
-    "fajr",
-    "sunrise",
-    "dhuhr",
-    "asr",
-    "maghrib",
-    "isha",
-  ] as const;
-
-  return prayerNames.map((prayer) => ({
-    name: prayer,
-    adhan: to12HourFormat(prayTimes[prayer]),
-    iqamah: getIqamahTime(prayer, hardcodedTimes, prayTimes, offsetTimes),
-  }));
-}
+import { initializeApp } from "firebase/app";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
 
 export type RegularPrayerNames =
   | "fajr"
@@ -29,38 +8,15 @@ export type RegularPrayerNames =
   | "asr"
   | "maghrib"
   | "isha";
-export function getIqamahTime(
-  timeName: RegularPrayerNames,
-  hardcodedTimes: HardcodedIqamahTimes,
-  prayTimes: NumberPrayerTimes,
-  offsetTimes: IqamahOffset,
-): string {
-  let hardcodedTime = hardcodedTimes[timeName];
-  if (hardcodedTime !== undefined) {
-    return hardcodedTime;
-  }
 
-  let time = prayTimes[timeName];
-  let offset = offsetTimes[timeName];
-  if (offset !== undefined) {
-    time += offset / 60;
-  }
-  return to12HourFormat(time);
-}
+export type OutputPrayerNames = RegularPrayerNames | "jumu'ah 1" | "jumu'ah 2";
 
-export function to12HourFormat(time: number): string {
-  let minutes = Math.round(time * 60);
-  let hours = Math.floor(minutes / 60) % 24;
-  minutes %= 60;
+/** Number of minutes from athan to iqamah */
+export type IqamahOffset = {
+  [name in RegularPrayerNames]?: number;
+};
 
-  let suffix = hours < 12 ? "am" : "pm";
-  hours = hours % 12 || 12;
-
-  return `${hours.toString().padStart(2, "0")}:${minutes
-    .toString()
-    .padStart(2, "0")} ${suffix}`;
-}
-
+export type HardcodedIqamahTimes = Partial<Record<OutputPrayerNames, string>>;
 export type PrayerData = {
   hardcodedIqamah: HardcodedIqamahTimes;
   iqamahOffset: IqamahOffset;
@@ -71,24 +27,18 @@ export type PrayerData = {
   }[];
 };
 
-/** Number of minutes from athan to iqamah */
-export type IqamahOffset = {
-  [name in OutputPrayerNames]?: number;
-};
+export async function getPrayerData(): Promise<PrayerData> {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCcoJg9kN0xqk54DdgJXLjBAvODeW01JVs",
+    authDomain: "up-ng-swinis-website.firebaseapp.com",
+    projectId: "up-ng-swinis-website",
+    storageBucket: "up-ng-swinis-website.appspot.com",
+    messagingSenderId: "864939860773",
+    appId: "1:864939860773:web:94b178435c2ca687ce70cc",
+  };
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
 
-export type OutputPrayerNames =
-  | "fajr"
-  | "sunrise"
-  | "dhuhr"
-  | "asr"
-  | "maghrib"
-  | "isha"
-  | "jumu'ah 1"
-  | "jumu'ah 2";
-
-export type HardcodedIqamahTimes = Partial<Record<OutputPrayerNames, string>>;
-
-export async function getPrayerData(db: Firestore): Promise<PrayerData> {
   const docRef = doc(db, "prayers", "prayerData");
   const docSnap = await getDoc(docRef);
 
